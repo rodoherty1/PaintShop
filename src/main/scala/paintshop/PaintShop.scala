8e98e3bc0c1ab21a910faaedc258fdbc5e0d7014
Rob O'Doherty <rodoherty1@gmail.com>
2014-07-06 22:00:13 +0100

    Just starting out ...

--- src/main/scala/paintshop/PaintShop.scala
+++ src/main/scala/paintshop/PaintShop.scala
@@ -1,7 +1,26 @@
 package paintshop
 
-object PaintShop {
+
+class PaintShop(n: Int) {
+    val MATTE = "M"
+    val GLOSS = "G"
     
-    def apply(orders: List[Map[Int, String]]): List[String] = ???
+    val PAINTTYPES = List(MATTE, GLOSS)
+    
+    val colours = 0 until n
+    
+    val batch = (colours map {i => i -> GLOSS}).toMap
+    
+    def apply(orders: List[Map[Int, String]]): List[String] = {
+        def satisfiesOrders (c: Int, p: String): Boolean = ???
+        
+        val batch = for {
+            c <- colours
+            p <- PAINTTYPES
+            if (satisfiesOrders(c, p))
+        } yield c + p
+        
+        ???
+    }
 
 }
--- src/test/scala/paintshop/PaintShopTest.scala
+++ src/test/scala/paintshop/PaintShopTest.scala
@@ -7,7 +7,7 @@ class PaintShopTest extends WordSpec with ShouldMatchers {
     
     "My Paintshop" should {
         "fulfill a simple order from one customer" in {
-            PaintShop(List(Map(1 -> "G"))) should be (List("G"))
+            new PaintShop(5)(List(Map(1 -> "G"))) should be (List("G"))
         } 
     }
 
