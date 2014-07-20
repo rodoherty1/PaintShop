2bb1c3f4f737ca2b4deb37941a8c0549be790bb1
Rob O'Doherty <rodoherty1@gmail.com>
2014-07-20 21:13:05 +0100

    Back on track!  Working for orders containing a single painttype

--- src/main/scala/paintshop/PaintShop.scala
+++ src/main/scala/paintshop/PaintShop.scala
@@ -1,26 +1,40 @@
 package paintshop
 
+object PaintShop {
+    type PaintType = Char
+    type CustomerOrder = Map[Int, PaintType]
+    
+	val MATTE = 'M'
+	val GLOSS = 'G'
+    
+    def generateBatches(batchSize: Int): Stream[List[PaintType]] = {
+        //val batch = List.fill(batchSize)(GLOSS)
+        Stream.cons(List[PaintType](GLOSS), Stream.cons(List[PaintType](MATTE), Stream()))
+    }
+    
+    
+}
 
-class PaintShop(n: Int) {
-    val MATTE = "M"
-    val GLOSS = "G"
+class PaintShop(batchSize: Int) {
+    import PaintShop._
     
-    val PAINTTYPES = List(MATTE, GLOSS)
+    val allBatches = generateBatches(batchSize)
     
-    val colours = 0 until n
-    
-    val batch = (colours map {i => i -> GLOSS}).toMap
-    
-    def apply(orders: List[Map[Int, String]]): List[String] = {
-        def satisfiesOrders (c: Int, p: String): Boolean = ???
-        
-        val batch = for {
-            c <- colours
-            p <- PAINTTYPES
-            if (satisfiesOrders(c, p))
-        } yield c + p
-        
-        ???
+    def apply(orders: List[CustomerOrder]): List[Char] = {
+
+        def isBatchSuitable(batch: List[PaintType], orders: List[CustomerOrder]) : Boolean = {
+            if (batch.length == 0 || orders.isEmpty) false
+            else (orders.forall(order => order.exists({ case (colour, paintType) => batch.contains(paintType)})))
+        }
+
+        def findBestBatch(batches: Stream[List[PaintType]]): List[PaintType] = {
+            if (batches.isEmpty) Nil
+            else {
+                if (isBatchSuitable(batches.head, orders)) batches.head
+                else findBestBatch(batches.tail)
+            }
+        }
+        findBestBatch(allBatches)
     }
 
 }
--- src/test/scala/paintshop/PaintShopTest.scala
+++ src/test/scala/paintshop/PaintShopTest.scala
@@ -6,8 +6,11 @@ import org.scalatest.WordSpec
 class PaintShopTest extends WordSpec with ShouldMatchers {
     
     "My Paintshop" should {
-        "fulfill a simple order from one customer" in {
-            new PaintShop(5)(List(Map(1 -> "G"))) should be (List("G"))
+        "fulfill a simple order from one customer who wants 1->G" in {
+            new PaintShop(1)(List(Map(1 -> 'G'))) should be (List('G'))
+        } 
+        "fulfill a simple order from one customer who wants 1->M" in {
+            new PaintShop(1)(List(Map(1 -> 'M'))) should be (List('M'))
         } 
     }
 
